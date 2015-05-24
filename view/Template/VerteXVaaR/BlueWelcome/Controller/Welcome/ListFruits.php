3c3229350c0783ef6e47c35467a9acff9cbb251b
Oliver Eglseder <php@vxvr.de>
2015-05-24 14:25:17 +0200

    [TASK] set correct link to index in listFruits

--- view/Template/VerteXVaaR/BlueWelcome/Controller/Welcome/ListFruits.php
+++ view/Template/VerteXVaaR/BlueWelcome/Controller/Welcome/ListFruits.php
@@ -92,6 +92,6 @@ $templateHelper->requireLayout('Basic/Html', ['pageTitle' => 'Hi dude!']);
     </p>
 
     <p>
-        <a href="">Back to the index</a>
+        <a href="./">Back to the index</a>
     </p>
 </section>
