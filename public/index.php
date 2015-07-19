030b748cc54b1e6d61d48acb67cde9799ca6e606
Oliver Eglseder <php@vxvr.de>
2015-07-19 11:21:30 +0200

    [TASK] set the default timezone to UTC

--- public/index.php
+++ public/index.php
@@ -4,15 +4,17 @@ define('VXVR_BS_ROOT', dirname(realpath(__DIR__)) . DIRECTORY_SEPARATOR);
 
 require_once(VXVR_BS_ROOT . 'vendor/autoload.php');
 
-if (PHP_SAPI === 'cli') {
+if (strlen(ini_get('date.timezone')) === 0) {
+    date_default_timezone_set('UTC');
+}
 
+if (PHP_SAPI === 'cli') {
     $scheduler = new \VerteXVaaR\BlueSprints\Task\Scheduler(
         \VerteXVaaR\BlueSprints\Task\CliRequest::createFromEnvironment()
     );
     $scheduler->run();
 } else {
     \VerteXVaaR\BlueSprints\Utility\Error::registerErrorHandler();
-
     $requestHandler = new \VerteXVaaR\BlueSprints\Http\RequestHandler();
     $response = $requestHandler->handleRequest(\VerteXVaaR\BlueSprints\Http\Request::createFromEnvironment());
     $response->respond();
