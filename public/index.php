fa5e051129cb42cffe1318e42016d6196209923c
Oliver Eglseder <php@vxvr.de>
2017-02-10 20:55:01 +0100

    [TASK] Update index.php from package

--- public/index.php
+++ public/index.php
@@ -1,9 +1,6 @@
 <?php
 
-define('VXVR_BS_APP', dirname(realpath(__DIR__)) . DIRECTORY_SEPARATOR);
-define('VXVR_BS_CONFIG', VXVR_BS_APP . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
-define('VXVR_BS_VIEW', VXVR_BS_APP . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
-define('VXVR_BS_ROOT', dirname(VXVR_BS_APP) . DIRECTORY_SEPARATOR);
+define('VXVR_BS_ROOT', dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR);
 
 require_once(VXVR_BS_ROOT . 'vendor/autoload.php');
 
@@ -11,12 +8,5 @@ if (empty(ini_get('date.timezone'))) {
     date_default_timezone_set('UTC');
 }
 
-if (PHP_SAPI === 'cli') {
-    $scheduler = new \VerteXVaaR\BlueSprints\Task\Scheduler(
-        \VerteXVaaR\BlueSprints\Task\CliRequest::createFromEnvironment()
-    );
-    $scheduler->run();
-} else {
-    \VerteXVaaR\BlueSprints\Utility\Error::registerErrorHandler();
-    \VerteXVaaR\BlueSprints\Http\Request::createFromEnvironment()->process()->respond();
-}
+\VerteXVaaR\BlueSprints\Utility\Error::registerErrorHandler();
+(new \VerteXVaaR\BlueSprints\Http\Request())->process()->respond();
