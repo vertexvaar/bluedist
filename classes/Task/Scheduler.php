e03be43dc0a3dffb45bad9370bbaeea7983b0140
Oliver Eglseder <php@vxvr.de>
2017-02-10 21:00:25 +0100

    [REFACTOR] Merge method to load scheduled tasks into the Schedulers constructor

--- classes/Task/Scheduler.php
+++ classes/Task/Scheduler.php
@@ -26,7 +26,7 @@ class Scheduler
     public function __construct(CliRequest $cliRequest)
     {
         $this->cliRequest = $cliRequest;
-        $this->tasks = $this->collectTasks();
+        $this->tasks = Files::requireFile('app/configuration/tasks.php');
     }
 
     /**
@@ -113,13 +113,5 @@ class Scheduler
     {
         $taskInformationFile = $this->getTaskInformationFile($taskName, $taskClassName);
         Files::writeFileContents($taskInformationFile, serialize($taskInformation));
-    }
-
-    /**
-     * @return array
-     */
-    protected function collectTasks(): array
-    {
-        return Files::requireFile('app/configuration/tasks.php');
     }
 }
