f3e5f3e7ab4d7574df1b9612f3c0728c77e78a74
Oliver Eglseder <php@vxvr.de>
2017-04-07 12:30:45 +0200

    [REFACTOR] Include original index.php insetad of duplicate maintaining

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-define('VXVR_BS_ROOT', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
-require_once(VXVR_BS_ROOT . 'vendor/autoload.php');
 
-if (empty(ini_get('date.timezone'))) {
-    date_default_timezone_set('UTC');
-}
-
-\VerteXVaaR\BlueSprints\Utility\Error::registerErrorHandler();
-(new \VerteXVaaR\BlueSprints\Http\Request())->process()->respond();
+define('VXVR_BS_ROOT', dirname(dirname(realpath(__DIR__))) . DIRECTORY_SEPARATOR);
+require('../../vendor/autoload.php');
+require('../../vendor/vertexvaar/bluesprints/resources/public/index.php');
