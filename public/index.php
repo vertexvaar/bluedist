b39f09e2c4d00f7fbe667c076226c50a516e27e5
Oliver Eglseder <php@vxvr.de>
2017-04-07 18:05:03 +0200

    [FEATURE] Add automatical symlinkin of the frameworks css folder

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!file_exists('css')) {
+    symlink('../../vendor/vertexvaar/bluesprints/resources/public/css/', 'css');
+}
+
 define('VXVR_BS_ROOT', dirname(dirname(realpath(__DIR__))) . DIRECTORY_SEPARATOR);
 require('../../vendor/autoload.php');
 require('../../vendor/vertexvaar/bluesprints/resources/public/index.php');
