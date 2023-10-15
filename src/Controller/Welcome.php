06ef26843b208ce903768912734cf6bfed96c2ce
Oliver Eglseder <php@vxvr.de>
2023-10-15 19:12:25 +0200

    [FEATURE] Render link to login/logout on the index page

--- src/Controller/Welcome.php
+++ src/Controller/Welcome.php
@@ -16,9 +16,12 @@ class Welcome extends AbstractController
 {
     #[Route(path: '/')]
     //#[Route(path: '/.*', priority: -1)]
-    public function index(): ResponseInterface
+    public function index(ServerRequestInterface $request): ResponseInterface
     {
-        return $this->render('fruits/index.html.twig', ['strings' => ['foo', 'bar', 'baz']]);
+        return $this->render('fruits/index.html.twig', [
+            'session' => $request->getAttribute('session'),
+            'strings' => ['foo', 'bar', 'baz']
+        ]);
     }
 
     #[Route(path: '/listFruits')]
