681b58a0e1185233d6d4dae87324a296cf6f59c8
Oliver Eglseder <php@vxvr.de>
2023-11-04 16:59:49 +0100

    [FEATURE] Add logic problem handling for action cache compilation

--- packages/blueweb/src/Caching/DependencyInjection/ActionCacheCompilerPass.php
+++ packages/blueweb/src/Caching/DependencyInjection/ActionCacheCompilerPass.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace VerteXVaaR\BlueWeb\Caching\DependencyInjection;
 
+use Exception;
 use ReflectionClass;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use VerteXVaaR\BlueWeb\Caching\Attributes\ActionCache;
 use VerteXVaaR\BlueWeb\Caching\Middleware\ActionCacheMiddleware;
+use VerteXVaaR\BlueWeb\Routing\Attributes\Route;
 
 use function array_keys;
 use function count;
@@ -32,6 +34,25 @@ class ActionCacheCompilerPass implements CompilerPassInterface
             foreach ($reflectionClass->getMethods() as $reflectionMethod) {
                 $reflectionCacheAttributes = $reflectionMethod->getAttributes(ActionCache::class);
                 if (1 === count($reflectionCacheAttributes)) {
+                    $reflectionRouteAttributes = $reflectionMethod->getAttributes(Route::class);
+                    if (empty($reflectionRouteAttributes)) {
+                        throw new Exception(
+                            'Can not cache an action without a route annotation.'
+                            . ' Method: ' . $class . '::' . $reflectionMethod->getName(),
+                        );
+                    }
+                    foreach ($reflectionRouteAttributes as $reflectionRouteAttribute) {
+                        /** @var Route $routeAttribute */
+                        $routeAttribute = $reflectionRouteAttribute->newInstance();
+                        if ($routeAttribute->method !== 'GET') {
+                            throw new Exception(
+                                'Can not cache actions with non-GET routes.'
+                                . ' Method: ' . $class . '::' . $reflectionMethod->getName()
+                                . ' Conflicting Route: ' . $routeAttribute->method . ': ' . $routeAttribute->path,
+                            );
+                        }
+                    }
+
                     $reflectionCacheAttribute = $reflectionCacheAttributes[0];
                     /** @var ActionCache $cacheAttribute */
                     $cacheAttribute = $reflectionCacheAttribute->newInstance();
--- src/Controller/Welcome.php
+++ src/Controller/Welcome.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace VerteXVaaR\BlueDist\Controller;
 
-use Exception;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerAwareInterface;
