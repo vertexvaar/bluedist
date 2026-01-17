786bff213c83f5341b107478dbf727405efe00c2
Oliver Eglseder <oliver.eglseder@co-stack.com>
2026-01-17 23:06:17 +0100

    [FEATURE] Add a command to change user passwords

--- packages/blueauth/config/commands.php
+++ packages/blueauth/config/commands.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 use VerteXVaaR\BlueAuth\Command\AddRoleToUserCommand;
+use VerteXVaaR\BlueAuth\Command\ChangeUserPasswordCommand;
 use VerteXVaaR\BlueAuth\Command\CreateUserCommand;
 
 return [
     CreateUserCommand::class,
+    ChangeUserPasswordCommand::class,
     AddRoleToUserCommand::class,
 ];
