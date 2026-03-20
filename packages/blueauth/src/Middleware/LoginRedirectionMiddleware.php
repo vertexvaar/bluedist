2e0c3983974cdfa7d997a2ceac0245ad179e615f
Oliver Eglseder <oliver.eglseder@co-stack.com>
2026-03-21 00:05:57 +0100

    [REFACTOR] Use smaller methods for increased readability in LoginRedirectionMiddleware

--- packages/blueauth/src/Middleware/LoginRedirectionMiddleware.php
+++ packages/blueauth/src/Middleware/LoginRedirectionMiddleware.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace VerteXVaaR\BlueAuth\Middleware;
 
+use CoStack\Lib\Exceptions\Ipv6SupportDisabledException;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\SimpleCache\CacheInterface;
@@ -27,49 +29,106 @@ readonly class LoginRedirectionMiddleware implements MiddlewareInterface
     {
         $response = $handler->handle($request);
 
-        if (
-            303 === $response->getStatusCode()
-            && '/login' === $request->getUri()->getPath()
-        ) {
-            $session = $request->getAttribute('session');
-            $cacheKey = 'previousRequest/' . $session->identifier;
-            if (
-                $session->isAuthenticated()
-                && $this->cache->has($cacheKey)
-            ) {
-                $previousRequestUri = $this->cache->get($cacheKey);
-                $this->cache->delete($cacheKey);
+        if ($this->isRedirectToLogin($request, $response)) {
+            $previousRequestUri = $this->getPreviousRequest($request);
+            if ($previousRequestUri) {
                 return new Response(303, ['Location' => $previousRequestUri]);
             }
+
             return $response;
         }
-        if (
-            403 === $response->getStatusCode()
-            && 'GET' === $request->getMethod()
-        ) {
+
+        if ($this->isAuthenticationRequired($request, $response)) {
             $session = $request->getAttribute('session');
+
+            // If authenticated but authentication is required, the user does not have the required authorization.
             if ($session->isAuthenticated()) {
                 return $response;
             }
-            $session = $this->authenticationService->forcePersistentSession($request);
-            $cacheKey = 'previousRequest/' . $session->identifier;
-            $uri = $request->getUri();
-            $remoteAddress = $request->getServerParams()['REMOTE_ADDR'];
-            if (inet_match_range($remoteAddress, $this->config->get('security.trustedProxyAddresses'))) {
-                if ($request->hasHeader('X-Forwarded-Proto')) {
-                    $forwardedProtocol = $request->getHeaderLine('X-Forwarded-Proto');
-                    if ('https' === $forwardedProtocol) {
-                        $uri = $uri->withScheme('https');
-                    }
-                }
-                if ($request->hasHeader('X-Forwarded-Port')) {
-                    $forwardedPort = $request->getHeaderLine('X-Forwarded-Port');
-                    $uri = $uri->withPort($forwardedPort);
-                }
-            }
-            $this->cache->set($cacheKey, $uri);
-            $response = new Response(303, ['Location' => '/login'], 'Login required');
+
+            $this->setPreviousRequest($request);
+
+            return new Response(303, ['Location' => '/login'], 'Login required');
         }
+
         return $response;
+    }
+
+    protected function isRedirectToLogin(ServerRequestInterface $request, ResponseInterface $response): bool
+    {
+        if (303 !== $response->getStatusCode()) {
+            return false;
+        }
+        if ('/login' !== $request->getUri()->getPath()) {
+            return false;
+        }
+        return true;
+    }
+
+    protected function getPreviousRequest(ServerRequestInterface $request): ?string
+    {
+        $session = $request->getAttribute('session');
+
+        if (!$session->isAuthenticated()) {
+            return null;
+        }
+
+        $cacheKey = 'previousRequest/' . $session->identifier;
+        if (!$this->cache->has($cacheKey)) {
+            return null;
+        }
+
+        $previousRequestUri = $this->cache->get($cacheKey);
+        $this->cache->delete($cacheKey);
+        return $previousRequestUri;
+    }
+
+    protected function isAuthenticationRequired(ServerRequestInterface $request, ResponseInterface $response): bool
+    {
+        if (403 !== $response->getStatusCode()) {
+            return false;
+        }
+        if ('GET' !== $request->getMethod()) {
+            return false;
+        }
+        return true;
+    }
+
+    protected function setPreviousRequest(ServerRequestInterface $request): void
+    {
+        $session = $this->authenticationService->forcePersistentSession($request);
+
+        $url = $this->getRedirectUrl($request);
+
+        $this->cache->set('previousRequest/' . $session->identifier, $url);
+    }
+
+    protected function getRedirectUrl(ServerRequestInterface $request): UriInterface
+    {
+        $uri = $request->getUri();
+
+        $remoteAddress = $request->getServerParams()['REMOTE_ADDR'];
+
+        try {
+            if (!inet_match_range($remoteAddress, $this->config->get('security.trustedProxyAddresses'))) {
+                return $uri;
+            }
+        } catch (Ipv6SupportDisabledException) {
+            return $uri;
+        }
+
+        if ($request->hasHeader('X-Forwarded-Proto')) {
+            $forwardedProtocol = $request->getHeaderLine('X-Forwarded-Proto');
+            if ('https' === $forwardedProtocol) {
+                $uri = $uri->withScheme('https');
+            }
+        }
+
+        if ($request->hasHeader('X-Forwarded-Port')) {
+            $forwardedPort = $request->getHeaderLine('X-Forwarded-Port');
+            $uri = $uri->withPort($forwardedPort);
+        }
+
+        return $uri;
     }
 }
