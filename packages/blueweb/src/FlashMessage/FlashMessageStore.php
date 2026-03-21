ada3cb569799d401e179a7e8da0f6362fd749b0b
Oliver Eglseder <oliver.eglseder@co-stack.com>
2026-03-21 01:08:52 +0100

    [BUGFIX] Store flash messages in subfolder, not prefixed

--- packages/blueweb/src/FlashMessage/FlashMessageStore.php
+++ packages/blueweb/src/FlashMessage/FlashMessageStore.php
@@ -11,7 +11,7 @@ class FlashMessageStore
 
     public function store(string|int $for, FlashMessage $flashMessage): void
     {
-        $flashMessages = $this->cache->get('flashMessage.' . $for, []);
+        $flashMessages = $this->cache->get('flashMessage/' . $for, []);
         $flashMessages[] = $flashMessage;
         $this->cache->set('flashMessage.' . $for, $flashMessages, new DateInterval('PT1M'));
     }
@@ -21,7 +21,7 @@ class FlashMessageStore
      */
     public function get(string|int $for): array
     {
-        $flashMessages = $this->cache->get('flashMessage.' . $for, []);
+        $flashMessages = $this->cache->get('flashMessage/' . $for, []);
         $this->cache->delete('flashMessage.' . $for);
         return $flashMessages;
     }
