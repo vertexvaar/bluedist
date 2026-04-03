55f44296b2355b69274e1cfd0756c3de6cd9bdee
Oliver Eglseder <oliver.eglseder@co-stack.com>
2026-04-03 21:22:07 +0200

    [BUGFIX] Fix fruits API controller and add pagination params

--- src/Controller/Api/FruitsController.php
+++ src/Controller/Api/FruitsController.php
@@ -4,23 +4,28 @@ namespace VerteXVaaR\BlueDist\Controller\Api;
 
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use VerteXVaaR\BlueDist\Model\Fruit;
 use VerteXVaaR\BlueSprints\Mvcr\Repository\Repository;
-use VerteXVaaR\BlueWeb\Controller\Controller;
+use VerteXVaaR\BlueWeb\Controller\Attribute\AsController;
 use VerteXVaaR\BlueWeb\Routing\Attributes\Route;
 
 use function json_encode;
 
-readonly class FruitsController implements Controller
+#[AsController]
+readonly class FruitsController
 {
     public function __construct(
         private Repository $repository,
     ) {}
 
     #[Route(path: '/api/fruits')]
-    public function index(): ResponseInterface
+    public function index(ServerRequestInterface $request): ResponseInterface
     {
-        $pagination = $this->repository->paginate(Fruit::class);
+        $page = $request->getQueryParams()['page'] ?? 1;
+        $perPage = $request->getQueryParams()['per_page'] ?? 10;
+
+        $pagination = $this->repository->paginate(Fruit::class, $page, $perPage);
 
         return new Response(200, ['Content-Type' => 'application/json'], json_encode($pagination));
     }
