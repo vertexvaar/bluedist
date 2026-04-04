abf56fd223f375df930ace91e5721b382623f6cf
Oliver Eglseder <oliver.eglseder@co-stack.com>
2026-04-04 11:49:50 +0200

    [BUGFIX] Fix storage and retrieval of flash messages

--- packages/blueweb/src/FlashMessage/FlashMessageStore.php
+++ packages/blueweb/src/FlashMessage/FlashMessageStore.php
@@ -5,15 +5,21 @@ namespace VerteXVaaR\BlueWeb\FlashMessage;
 use DateInterval;
 use Psr\SimpleCache\CacheInterface;
 
-class FlashMessageStore
-{
-    public function __construct(protected readonly CacheInterface $cache) {}
+use function serialize;
+use function unserialize;
 
-    public function store(string|int $for, FlashMessage $flashMessage): void
+readonly class FlashMessageStore
+{
+    public function __construct(protected CacheInterface $cache) {}
+
+    public function store(string|int $for, FlashMessage $flashMessage, DateInterval|int|null $ttl = null): void
     {
-        $flashMessages = $this->cache->get('flashMessage/' . $for, []);
-        $flashMessages[] = $flashMessage;
-        $this->cache->set('flashMessage.' . $for, $flashMessages, new DateInterval('PT1M'));
+        $key = $this->getKey($for);
+
+        $flashMessages = $this->cache->get($key, []);
+        $flashMessages[] = serialize($flashMessage);
+
+        $this->cache->set($key, $flashMessages, $ttl ?? new DateInterval('PT1M'));
     }
 
     /**
@@ -21,8 +27,19 @@ class FlashMessageStore
      */
     public function get(string|int $for): array
     {
-        $flashMessages = $this->cache->get('flashMessage/' . $for, []);
-        $this->cache->delete('flashMessage.' . $for);
+        $key = $this->getKey($for);
+
+        $flashMessages = $this->cache->get($key, []);
+        $this->cache->delete($key);
+
+        foreach ($flashMessages as $index => $flashMessage) {
+            $flashMessages[$index] = unserialize($flashMessage, ['allowed_classes' => [FlashMessage::class]]);
+        }
         return $flashMessages;
+    }
+
+    protected function getKey(int|string $for): string
+    {
+        return 'flashMessage/' . $for;
     }
 }
