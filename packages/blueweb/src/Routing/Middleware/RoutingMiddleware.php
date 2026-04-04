fe09bd21eb4d76dc6522fd897cbcd3e37e6b0b2a
Oliver Eglseder <oliver.eglseder@co-stack.com>
2026-04-04 11:51:18 +0200

    [BUGFIX] Return the correct status code when routing to invalid or non existent actions

--- packages/blueweb/src/Routing/Middleware/RoutingMiddleware.php
+++ packages/blueweb/src/Routing/Middleware/RoutingMiddleware.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace VerteXVaaR\BlueWeb\Routing\Middleware;
 
-use Exception;
 use FastRoute\Dispatcher;
 use FastRoute\Dispatcher\GroupCountBased;
+use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -16,6 +16,8 @@ use VerteXVaaR\BlueWeb\Exception\RouteAttributeConstructionException;
 use VerteXVaaR\BlueWeb\Middleware\Attribute\AsMiddleware;
 use VerteXVaaR\BlueWeb\Routing\RouteEncapsulation;
 
+use function array_merge;
+
 #[AsMiddleware('vertexvaar/bluesprints/routing')]
 readonly class RoutingMiddleware implements MiddlewareInterface
 {
@@ -23,6 +25,9 @@ readonly class RoutingMiddleware implements MiddlewareInterface
         private array $data,
     ) {}
 
+    /**
+     * @throws RouteAttributeConstructionException
+     */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $path = $request->getUri()->getPath();
@@ -30,33 +35,33 @@ readonly class RoutingMiddleware implements MiddlewareInterface
 
         $dispatcher = new GroupCountBased($this->data);
         $routingResult = $dispatcher->dispatch($method, $path);
-        $matchedRoute = match ($routingResult[0]) {
-            Dispatcher::FOUND => $routingResult[1],
-            Dispatcher::NOT_FOUND => throw new Exception(
-                'Could not resolve a route for path "' . $path . '"',
-                1431887428,
-            ),
-            Dispatcher::METHOD_NOT_ALLOWED => throw new Exception(
-                'Method not allowed for route "' . $path . '"',
-                1699381583,
-            ),
-        };
+        [$status, $routeHandler, $matches] = array_merge($routingResult, [null, null]);
+
+        if (Dispatcher::NOT_FOUND === $status) {
+            return new Response(404, [], 'Could not resolve a route for path "' . $path . '"');
+        }
+        if (Dispatcher::METHOD_NOT_ALLOWED === $status) {
+            return new Response(405, [], 'Method not allowed for route "' . $path . '"');
+        }
+
         try {
             $routeEncapsulation = new RouteEncapsulation(
-                new ($matchedRoute['class'])(...$matchedRoute['vars']),
-                $matchedRoute['controller'],
-                $matchedRoute['action'],
-                $routingResult[2],
+                new ($routeHandler['class'])(...$routeHandler['vars']),
+                $routeHandler['controller'],
+                $routeHandler['action'],
+                $matches,
             );
         } catch (TypeError $exception) {
             throw new RouteAttributeConstructionException(
-                $matchedRoute['class'],
-                $matchedRoute['controller'],
-                $matchedRoute['action'],
+                $routeHandler['class'],
+                $routeHandler['controller'],
+                $routeHandler['action'],
                 $exception,
             );
         }
+
         $request = $request->withAttribute('route', $routeEncapsulation);
+
         return $handler->handle($request);
     }
 }
